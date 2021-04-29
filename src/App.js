337170d3eef1ba8ef79c448cdd93eb61c8267867
“ZHOU125disorder” <3440681909@qq.com>
2021-04-29 16:43:06 +0800

    The_First_React_Dome_Have_Over_Hello_World

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header/index';
-import Detail from './pages/detail/loadable';
+import Detail from './pages/detail/loadable.js';
 import Home from './pages/home';
 import login from './pages/login';
 import write from './pages/write';
